c1e7d282e1786a123f4e45bb77aae058e7fd86e8
blomdhal <shoon95@gmail.com>
2015-04-13 15:53:09 +0900

    2015-04-13, First commit

--- README.md
+++ README.md
@@ -1,2 +1,221 @@
-# vendormac
-Simple build vendor mac map files from http://standards-oui.ieee.org/oui.txt
+Vendormac
+=========
+
+ This is simple utility for generating Vendor Mac Map such as Propery file, xml file and apple enum elements.
+
+Precondition
+------------
+
+ 1. Source file from http://standards-oui.ieee.org/oui.txt.
+ 2. Save as IEEE-oui.txt(Default source file name)
+> NOTE: Periodically Need to update http://standards-oui.ieee.org/oui.txt 
+
+
+Usage
+-----
+
+```
+cmd> java -jar vendormac.jar <source file>
+```
+
+*Example*
+```
+cmd> java -jar vendormac.jar IEEE-oui.txt
+```
+
+Output files
+------------
+
+* apple-enum-generated.txt
+	- enum elements for Apple Mac
+* vendorMacs-generated.prop
+	- '=' Seperated file format
+* vendorMacs-generated.xml
+	- xml file format
+
+applee-enum-generated.txt
+-----------------------------
+
+*File contents*
+```
+$000393("000393", "Apple"),
+$000502("000502", "Apple"),
+$000A27("000A27", "Apple"),
+$000A95("000A95", "Apple"),
+$000D93("000D93", "Apple"),
+$0010FA("0010FA", "Apple"),
+$001124("001124", "Apple"),
+$001451("001451", "Apple"),
+$0016CB("0016CB", "Apple"),
+$0017F2("0017F2", "Apple"),
+(snip)...
+```
+
+
+*Usage example*
+```java
+public enum AppleMacs {
+    // Here, Copy and Paste from applee-enum-generated.txt
+    $000393("000393", "Apple"),
+    $000502("000502", "Apple"),
+    $000A27("000A27", "Apple"),
+    (snip)...
+     ;
+    
+    private static final Map<String, AppleMacs> kMap;
+    static {
+        kMap = new HashMap<String, AppleMacs>();
+        
+        for (AppleMacs k : AppleMacs.values()) {
+            kMap.put(k.prefix, k);
+        }
+    }
+    
+    public static AppleMacs getEnum(String prefix) {
+        return kMap.get(prefix);
+    }
+    
+    public final String prefix;
+    public final String vendor;
+    AppleMacs(String prefix, String vendor) {
+        this.prefix = prefix;
+        this.vendor = vendor;
+    }
+    
+    @Override
+    public String toString() {
+        return "prefix(" + prefix + ") , vendor(" + vendor + ")";
+    }
+}
+```
+
+vendorMacs-generated.prop
+-------------------------
+'=' Seperated file.
+
+*File contents*
+```
+000000=XEROX CORPORATION
+000001=XEROX CORPORATION
+000002=XEROX CORPORATION
+000003=XEROX CORPORATION
+000004=XEROX CORPORATION
+000005=XEROX CORPORATION
+000006=XEROX CORPORATION 
+(snip)...
+```
+
+*Usage example*
+```java
+    Properties macs = new Properties();
+
+   try {
+        FileInputStream vendorMacs = new FileInputStream("vendorMacs-generated.prop");
+        if (vendorMacs != null) {
+            try {
+                macs.load(vendorMacs);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            vendorMacs.close();
+            vendorMacs = null;
+        }
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    } 
+```
+
+vendorMacs-generated.xml
+------------------------
+xml file format.
+
+*File content*
+```
+<?xml version='1.0' standalone='yes' ?>
+<!--癤? Generated: Tue, 07 Apr 2015 05:00:04 -0400-->
+<MacAddressVendorMappings xmlns="http://standards-oui.ieee.org/oui.txt">
+  <VendorMapping mac_prefix="000000" vendor_name="XEROX CORPORATION" />
+  <VendorMapping mac_prefix="000001" vendor_name="XEROX CORPORATION" />
+  <VendorMapping mac_prefix="000002" vendor_name="XEROX CORPORATION" />
+  <VendorMapping mac_prefix="000003" vendor_name="XEROX CORPORATION" />
+  (snip)...
+  <VendorMapping mac_prefix="FCFFAA" vendor_name="IEEE REGISTRATION AUTHORITY  - Please see MAL public listing for more information." />
+</MacAddressVendorMappings>  
+```
+
+*Usage example*
+
+```java
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+public class VendorMacsMap {    
+    private static final String DEFAULT_PATH = "/system/etc/vendorMacs-generated.xml";
+    private static final Map<String, String> kVedorMacsMaps = new HashMap<String, String>();
+    
+    private static final String PATH;
+    
+    static {
+        PATH = DEFAULT_PATH;
+        
+        try {
+            int eventType = 0;
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(PATH)));
+
+            xpp.setInput(br);
+            while ((eventType = xpp.next()) != XmlPullParser.END_DOCUMENT) {
+                
+                switch (eventType) {
+                case XmlPullParser.START_TAG:
+                    if ("VendorMapping".equals(xpp.getName())) {
+                        String mac = xpp.getAttributeValue(null, "mac_prefix");
+                        String vendor = xpp.getAttributeValue(null, "vendor_name");
+                        kVedorMacsMaps.put(mac, vendor);
+                    }
+                    break;
+                }
+            }
+        } catch (XmlPullParserException e) {
+            ALog.e(TAG, "XmlPullParserException exception!");
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            ALog.e(TAG, "File Not Found: " + PATH);
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    static public String getVendorName(String mac) {
+        
+        // start of 6 chars in address
+        String mac_prefix = (mac.contains(":") == true) ? mac.substring(0, 8)
+                : mac.substring(0, 6);
+        StringTokenizer st = new StringTokenizer(mac_prefix, ":");
+        String key = "";
+        
+        while(st.hasMoreTokens())
+            key += st.nextToken();
+        
+        return kVedorMacsMaps.get(key);
+    }
+}
+```
---
+++ src/com/sdc/shjung/VendorMac.java
@@ -0,0 +1,203 @@
+package com.sdc.shjung;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+import org.xmlpull.v1.XmlSerializer;
+
+public class VendorMac {
+
+    private static final String DEFAULT_SOURCE_PATH = "IEEE-oui.txt";
+    private static final String DEFAULT_DEST_PATH = "vendorMacs-generated.prop";
+    private static final String APPLE_ENUM_PATH = "applee-enum-generated.txt";
+    private static final String VENDOR_MACS_PATH = "vendorMacs-generated.xml";
+    
+    private static final String DELIM = "(base 16)";
+    private static final String EQUAL = "=";
+    
+    private static void printUsage() {
+        System.out.println("Usage: java -jar vendormac.jar <source file>");
+    }
+    
+    // '=' serated file
+    private static void loadOUIAndWrite(final String source) throws IOException {
+        
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(source)));
+        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(DEFAULT_DEST_PATH)));
+        String tmp = null;
+        
+        bfw.append("* Reference from: http://standards-oui.ieee.org/oui.txt").append("\n");
+        bfw.append("*" + bfr.readLine()).append("\n\n");
+        
+        
+        while ( (tmp = bfr.readLine()) != null) {
+            if(tmp.contains(DELIM) == false)
+                continue;
+            
+            StringTokenizer stk = new StringTokenizer(tmp.replace(DELIM, "="), EQUAL);
+            String prefix = null;
+            String vendor = null;
+            
+            if (stk.hasMoreTokens())
+                prefix = stk.nextToken().trim();
+            
+            if (stk.hasMoreTokens())
+                vendor = stk.nextToken().trim();
+            
+            // Make '=' separated
+            bfw.append(prefix).append("=").append(vendor).append("\n");
+        }
+        
+        bfr.close();
+        bfw.close();
+        
+        System.out.println("Done!!!, File generated : " + DEFAULT_DEST_PATH);
+    }
+    
+    private static void loadOUIAndmakeAppleMacEnumElements(final String source) throws IOException {
+        
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(source)));
+        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(APPLE_ENUM_PATH)));
+        String tmp = null;
+        
+        bfw.append("* Reference from: http://standards-oui.ieee.org/oui.txt").append("\n");
+        bfw.append("* " + bfr.readLine()).append("\n\n");
+        
+        
+        while ( (tmp = bfr.readLine()) != null) {
+            if (tmp.contains(DELIM) == false)
+                continue;
+            
+            if (tmp.contains("Apple") == false)
+                continue;
+            
+            StringTokenizer stk = new StringTokenizer(tmp.replace(DELIM, "="), EQUAL);
+            String prefix = null;
+            String vendor = null;
+            
+            if (stk.hasMoreTokens())
+                prefix = stk.nextToken().trim();
+            
+            if (stk.hasMoreTokens())
+                vendor = stk.nextToken().trim();
+            
+            // Make enum member
+            bfw.append("$").append(prefix).append("(\"").append(prefix).append("\", ");
+            bfw.append("\"").append(vendor).append("\"),").append("\n");
+        }
+        
+        bfr.close();
+        bfw.close();
+        
+        System.out.println("Done!!!, File generated : " + APPLE_ENUM_PATH);
+        System.out.println("You can Copy and Paste to com.lge.aha.constants.AppleMacs.java");
+    }    
+
+    private static void loadOUIAndmakeXml(final String source) throws IOException, XmlPullParserException {
+        
+        final String NAMESPACE = "http://standards-oui.ieee.org/oui.txt";
+        
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance(
+                System.getProperty(XmlPullParserFactory.PROPERTY_NAME), null);
+        XmlSerializer serializer = factory.newSerializer();
+        
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(source)));
+        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(VENDOR_MACS_PATH)));
+        String tmp = null;
+        
+        // xml
+        serializer.setOutput(bfw);
+        serializer.startDocument(null, Boolean.valueOf(true));
+        serializer.ignorableWhitespace("\n");
+        serializer.comment(bfr.readLine());
+        serializer.ignorableWhitespace("\n");
+        
+        serializer.setPrefix("", NAMESPACE);
+        serializer.startTag(NAMESPACE, "MacAddressVendorMappings");
+        //serializer.text("\n");
+        
+        while ( (tmp = bfr.readLine()) != null) {
+            if (tmp.contains(DELIM) == false)
+                continue;
+            
+            StringTokenizer stk = new StringTokenizer(tmp.replace(DELIM, "="), EQUAL);
+            String prefix = null;
+            String vendor = null;
+            
+            if (stk.hasMoreTokens())
+                prefix = stk.nextToken().trim();
+            
+            if (stk.hasMoreTokens())
+                vendor = stk.nextToken().trim();
+            
+            // xml attribute
+            writeLine(serializer, prefix, vendor);
+        }
+        
+        serializer.endTag(NAMESPACE, "MacAddressVendorMappings");
+        serializer.endDocument();
+        
+        //write xml data into the FileOutputStream
+        serializer.flush();
+        
+        bfr.close();
+        bfw.close();
+        
+        System.out.println("Done!!!, File generated : " + VENDOR_MACS_PATH);
+    }
+    
+    private static void writeLine(XmlSerializer serializer, String prefix, String vendor)
+            throws IOException {
+        
+        // set indentation option
+        serializer.setFeature(
+                "http://xmlpull.org/v1/doc/features.html#indent-output", true);
+        
+        serializer.startTag(null, "VendorMapping");
+        serializer.attribute(null, "mac_prefix", prefix);
+        serializer.attribute(null, "vendor_name", vendor.replaceAll("&", ""));
+        serializer.endTag(null, "VendorMapping");
+//        serializer.text("\n");
+    }
+    
+    /**
+     * @param args
+     * @throws IOException 
+     * @throws XmlPullParserException 
+     */
+    public static void main(String[] args) throws IOException, XmlPullParserException {
+        
+        String src = null;
+        
+        System.out.println("Generate vendor mac file from RAW IEEE-oui files(http://standards-oui.ieee.org/oui.txt)");
+        System.out.println("");
+        
+        if (args.length != 1) {
+            printUsage();
+            return;
+        }
+        
+        if ( new File(args[0]).isFile() == true)
+            src = args[0];
+        else {
+            System.out.println(args[0] + " does not exist. use default source file: " + DEFAULT_SOURCE_PATH);
+            src = DEFAULT_SOURCE_PATH;
+        }
+        
+        System.out.println("");
+        
+        loadOUIAndWrite(src);
+        loadOUIAndmakeAppleMacEnumElements(src);
+        loadOUIAndmakeXml(src);
+    }
+
+}
